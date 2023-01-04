8558de6184c2e8c1284d3c5f50eea093971e43ad
hawaii <137668355@qq.com>
2023-01-04 11:37:08 +0800

    同步更新若干库，更新检测关键词列表

--- cyuyan_reviewer.py
+++ cyuyan_reviewer.py
@@ -1,6 +1,6 @@
 import argparse
 import asyncio
-import aiohttp
+import httpx
 import re
 from typing import Optional, Union
 
@@ -20,8 +20,9 @@ class MyReviewer(tb.Reviewer):
             re.compile('http://8.136.190.216:8080/ca'),
             re.compile('867056058'),
             re.compile('1259845250'),
-            re.compile('3058256485'),
             re.compile('graves2022'),
+            re.compile('❻❼❾❸❹❾❻'),
+            re.compile('昆鹏发卡'),
         ]
 
     def time_interval(self):
@@ -32,9 +33,9 @@ class MyReviewer(tb.Reviewer):
         根据违规次数，返回对应的封禁理由
         """
         if violations < 8:
-            return '散布代写/接单/辅导/助考类广告'
+            return '散布代写/接单/辅导类主题帖，或推广网站/产品/群聊等广告行为'
         elif 8 <= violations <= 10:
-            return f'散布代写/接单/辅导/助考类广告；请注意，你已有{violations}次违规记录，请阅读并遵守吧规。继续违规可能导致永久删封处罚。'
+            return f'散布代写/接单/辅导类主题帖，或推广网站/产品/群聊等广告行为；请注意，你已有{violations}次违规记录，请阅读并遵守吧规。继续违规可能导致永久删封处罚。'
         else:
             return '无视吧规多次散布广告，屡教不改，情节恶劣，予以发言永久删封处罚。'
 
@@ -45,21 +46,20 @@ class MyReviewer(tb.Reviewer):
 
         # 机器学习判断广告内容
         if not thread.is_top and not thread.is_good:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url=antispammer_url, data={'text': thread.text}) as resp:
-                    result = await resp.text()
-                    if result == 'spam':
-                        punish = True
+            async with httpx.AsyncClient() as client:
+                r = await client.post(antispammer_url, data={'text': thread.text})
+                if r.text == 'spam':
+                    punish = True
         # 判断违规图片
         for img_content in thread.contents.imgs:
-            img = await self.client.get_image(img_content.src)
+            img = await self.client.get_image(img_content.origin_src)
             if img.size == 0:
                 continue
-            permission = await self.get_imghash(img, hamming_distance=5)
+            permission = await self.get_imghash(img, hamming_dist=5)
             if permission <= -5:
                 punish = True
                 phash = self.compute_imghash(img)
-                LOG.info(f'Possible spam image: src={img_content.src}, img_hash={img_content.hash}, phash={phash}')
+                LOG.info(f'Possible spam image: src={img_content.origin_src}, img_hash={img_content.hash}, phash={phash}')
                 break
         
         if punish:
