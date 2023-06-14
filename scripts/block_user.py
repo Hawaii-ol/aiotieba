bf7d2f4e0a00de42325aa4a46b7513d1abded6c4
hawaii <137668355@qq.com>
2023-06-14 14:49:05 +0800

    query_user.py和block_user.py支持tieba_uid

--- scripts/block_user.py
+++ scripts/block_user.py
@@ -1,24 +1,45 @@
 import __init__
 import sys
 import asyncio
+import argparse
 import aiotieba as tb
 from cyuyan_reviewer import FraudTypes, punish_note
 
-async def main(fname):
-    async with tb.Reviewer('default', fname) as reviewer:
-        if len(sys.argv) == 1:
-            credential = input('Enter {id|portrait|user_name}: ')
-        else:
-            credential = sys.argv[1]
+async def main(fname, credential, cred_type=None):
+    if not cred_type:
+        try:
+            credential = int(credential)
+            print('Credential type not specified and what you enter seems to be an integer. '
+                  'Please select the credential type you would like to query by:')
+            print('[1] user_id')
+            print('[2] user_name')
+            print('[3] tieba_uid')
+            choice = int(input())
+            if choice in [1, 2, 3]:
+                if choice == 2:
+                    credential = str(credential)
+                elif choice == 3:
+                    cred_type = 'tieba_uid'
+            else:
+                print('Invalid choice.')
+                exit(1)
+        except ValueError:
+            pass
+    elif cred_type in ['user_id', 'tieba_uid']:
         try:
             credential = int(credential)
         except ValueError:
-            pass
-        user = await reviewer.client.get_user_info(credential)
+            print(f"{cred_type} must be an integer, not '{credential}'.")
+            exit(1)
+    async with tb.Reviewer('default', fname) as reviewer:
+        if cred_type == 'tieba_uid':
+            user = await reviewer.client.tieba_uid2user_info(credential)
+        else:
+            user = await reviewer.client.get_user_info(credential)
         print('请选择封禁原因：')
-        print('1.广告')
-        print('2.疑似诈骗')
-        print('3.举报核实诈骗')
+        print('[1] 广告')
+        print('[2] 疑似诈骗')
+        print('[3] 举报核实诈骗')
         reason = int(input())
         if reason in [1, 2, 3]:
             if credit := await reviewer.db.get_user_credit(user):
@@ -36,7 +57,16 @@ async def main(fname):
             await reviewer.db.add_user_credit(user, fraud_type == FraudTypes.CONFIRMED_FRAUD)
             await reviewer.block(user.portrait, day=1, reason=note)
         else:
-            print('无效选项')
+            print('Invalid choice.')
+            exit(1)
 
 if __name__ == '__main__':
-    asyncio.run(main('C语言'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t','--cred_type',
+        help='credential type',
+        choices=['user_id', 'user_name', 'portrait', 'tieba_uid'],
+    )
+    parser.add_argument('credential')
+    args = parser.parse_args()
+    asyncio.run(main('C语言', args.credential, args.cred_type))
--- scripts/query_user.py
+++ scripts/query_user.py
@@ -1,25 +1,47 @@
 import __init__
-import sys
 import asyncio
 import aiotieba
+import argparse
 
 
-async def main():
-    async with aiotieba.Client('default') as client:
-        if len(sys.argv) == 1:
-            credential = input('Enter {id|portrait|user_name}: ')
-        else:
-            credential = sys.argv[1]
+async def main(credential, cred_type=None):
+    if not cred_type:
+        try:
+            credential = int(credential)
+            print('Credential type not specified and what you enter seems to be an integer. '
+                  'Please select the credential type you would like to query by:')
+            print('[1] user_id')
+            print('[2] user_name')
+            print('[3] tieba_uid')
+            choice = int(input())
+            if choice in [1, 2, 3]:
+                if choice == 2:
+                    credential = str(credential)
+                elif choice == 3:
+                    cred_type = 'tieba_uid'
+            else:
+                print('Invalid choice.')
+                exit(1)
+        except ValueError:
+            pass
+    elif cred_type in ['user_id', 'tieba_uid']:
         try:
             credential = int(credential)
         except ValueError:
-            pass
-        user = await client.get_user_info(credential)
+            print(f"{cred_type} must be an integer, not '{credential}'.")
+            exit(1)
+    async with aiotieba.Client('default') as client:
+        if cred_type == 'tieba_uid':
+            user = await client.tieba_uid2user_info(credential)
+            user = await client.get_user_info(user.portrait)
+        else:
+            user = await client.get_user_info(credential)
         print_uinfo(user)
 
 def print_uinfo(user):
     print(f'user_id: {user.user_id}')
     print(f'portrait: {user.portrait}')
+    print(f'贴吧ID: {user.tieba_uid}')
     print(f'用户名: {user.user_name}')
     print(f'昵称: {user.nick_name}')
     print(f'性别: {("未知", "男", "女")[user.gender]} (gender: {user.gender})')
@@ -30,4 +52,12 @@ def print_uinfo(user):
     print(f'ip归属地: {user.ip}')
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description='query user info by several credential types')
+    parser.add_argument(
+        '-t','--cred_type',
+        help='credential type',
+        choices=['user_id', 'user_name', 'portrait', 'tieba_uid'],
+    )
+    parser.add_argument('credential')
+    args = parser.parse_args()
+    asyncio.run(main(args.credential, args.cred_type))
